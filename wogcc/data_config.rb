81d701f10755c45fe8e875b12512e973712c0733
airwatergas <troy.burke@colorado.edu>
2016-07-28 12:14:08 -0600

    wogcc folder

---
+++ wogcc/data_config.rb
@@ -0,0 +1,23 @@
+def getMappings
+  "/Users/troyburke/Data/wogcc/scripts/models/"
+end
+
+def getDBHost
+  "localhost"
+end
+
+def getDBPort
+  "5432"
+end
+
+def getDBUsername
+  "wogcc"
+end
+
+def getDBDatabase
+  "wogcc_development"
+end
+
+def getDBSchema
+  "public"
+end
