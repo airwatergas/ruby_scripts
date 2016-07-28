b9aea66523e3275089dbb060775bc39fc7323ba7
airwatergas <troy.burke@colorado.edu>
2016-07-28 12:05:51 -0600

    data config files

---
+++ cogcc_data_config.rb
@@ -0,0 +1,19 @@
+def getMappings
+  "/Users/troyburke/Projects/ruby/models/"
+end
+
+def getDBHost
+  "localhost"
+end
+
+def getDBPort
+  "5432"
+end
+
+def getDBUsername
+  "cogcc"
+end
+
+def getDBDatabase
+  "cogcc_development"
+end
