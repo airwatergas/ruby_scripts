40a9093ec303c033293b2195813c8c730aab301c
airwatergas <troy.burke@colorado.edu>
2016-07-28 12:18:44 -0600

    cogcc folder

---
+++ cogcc/cogcc_status_codes.sql
@@ -0,0 +1,66 @@
+create table cogcc_status_codes (
+	id serial not null primary key, 
+	name varchar(30),
+	code varchar(2), 
+	type varchar(20), -- well, wellbore, formation treatment, surface location
+	description varchar(500)
+);
+
+insert into cogcc_status_codes (id, name, code, type) values (, '', '', 'well');
+insert into cogcc_status_codes (id, name, code, type) values (, '', '', 'well');
+insert into cogcc_status_codes (id, name, code, type) values (, '', '', 'well');
+insert into cogcc_status_codes (id, name, code, type) values (, '', '', 'well');
+insert into cogcc_status_codes (id, name, code, type) values (, '', '', 'well');
+insert into cogcc_status_codes (id, name, code, type) values (, '', '', 'well');
+insert into cogcc_status_codes (id, name, code, type) values (, '', '', 'well');
+insert into cogcc_status_codes (id, name, code, type) values (, '', '', 'well');
+insert into cogcc_status_codes (id, name, code, type) values (, '', '', 'well');
+insert into cogcc_status_codes (id, name, code, type) values (, '', '', 'well');
+insert into cogcc_status_codes (id, name, code, type) values (, '', '', 'well');
+insert into cogcc_status_codes (id, name, code, type) values (, '', '', 'well');
+insert into cogcc_status_codes (id, name, code, type) values (, '', '', 'well');
+insert into cogcc_status_codes (id, name, code, type) values (, '', '', 'well');
+
+insert into cogcc_status_codes (id, name, code, type) values (, '', '', 'wellbore');
+insert into cogcc_status_codes (id, name, code, type) values (, '', '', 'wellbore');
+insert into cogcc_status_codes (id, name, code, type) values (, '', '', 'wellbore');
+insert into cogcc_status_codes (id, name, code, type) values (, '', '', 'wellbore');
+insert into cogcc_status_codes (id, name, code, type) values (, '', '', 'wellbore');
+insert into cogcc_status_codes (id, name, code, type) values (, '', '', 'wellbore');
+insert into cogcc_status_codes (id, name, code, type) values (, '', '', 'wellbore');
+insert into cogcc_status_codes (id, name, code, type) values (, '', '', 'wellbore');
+insert into cogcc_status_codes (id, name, code, type) values (, '', '', 'wellbore');
+insert into cogcc_status_codes (id, name, code, type) values (, '', '', 'wellbore');
+insert into cogcc_status_codes (id, name, code, type) values (, '', '', 'wellbore');
+
+insert into cogcc_status_codes (id, name, code, type) values (, '', '', 'formation treatment');
+insert into cogcc_status_codes (id, name, code, type) values (, '', '', 'formation treatment');
+insert into cogcc_status_codes (id, name, code, type) values (, '', '', 'formation treatment');
+insert into cogcc_status_codes (id, name, code, type) values (, '', '', 'formation treatment');
+insert into cogcc_status_codes (id, name, code, type) values (, '', '', 'formation treatment');
+insert into cogcc_status_codes (id, name, code, type) values (, '', '', 'formation treatment');
+insert into cogcc_status_codes (id, name, code, type) values (, '', '', 'formation treatment');
+insert into cogcc_status_codes (id, name, code, type) values (, '', '', 'formation treatment');
+
+insert into cogcc_status_codes (id, name, code, type) values (, '', '', 'surface location');
+insert into cogcc_status_codes (id, name, code, type) values (, '', '', 'surface location');
+insert into cogcc_status_codes (id, name, code, type) values (, '', '', 'surface location');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
